59acfe85e6b59114e2789a09be0e130d238c889a
Ian Reese <kalissaac@protonmail.com>
2020-06-08 11:23:15 -0700

    Add basis for reaction listener

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use serenity::client::Client;
-use serenity::model::{channel::Message, channel::Reaction, gateway::Ready};
+use serenity::model::{channel::Message, channel::Reaction, gateway::Ready, id::{ChannelId, MessageId}};
 use serenity::prelude::{EventHandler, Context};
 use serenity::framework::standard::{
     StandardFramework,
-    CommandResult,
+    Args, CommandResult,
     macros::{
         command,
         group
@@ -12,7 +12,7 @@ use serenity::framework::standard::{
 extern crate dotenv;
 
 #[group]
-#[commands(ping)]
+#[commands(ping, track_message)]
 struct General;
 
 use dotenv::dotenv;
@@ -48,6 +48,22 @@ fn main() {
 #[command]
 fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!")?;
+
+    Ok(())
+}
+
+#[command]
+fn track_message(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    let message_id = match args.single::<u64>() {
+        Ok(id) => MessageId(id),
+        Err(_) => {
+            msg.reply(&ctx, "Requires a valid message ID be given").expect("Message failed");
+
+            return Ok(());
+        },
+    };
+
+    msg.reply(ctx, &format!("Reaction listener added for message {}", message_id))?;
 
     Ok(())
 }
