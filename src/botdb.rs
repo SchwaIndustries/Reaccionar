98d3da3872c87cda1b2f31bc268a4dd14bf93915
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-06-21 11:38:45 -0700

    Added setter function to the database code

--- src/botdb.rs
+++ src/botdb.rs
@@ -1,6 +1,7 @@
 extern crate redis;
 use redis::Commands;
 use serenity::model::id::GuildId;
+use std::collections::HashMap;
 
 mod botdb {
 
@@ -19,12 +20,22 @@ mod botdb {
 
     pub fn get_guild(connection: redis::RedisResult<Connection>, guild_id: u64) -> DB_Entry {
         let map : HashMap<String, u64> = connection.hgetall(guild_id.to_string())?;
-        let entry = DB_Entry;
+        let mut entry = DB_Entry;
         entry.guild_id = map.get(&"guild_id");
         entry.message_id = map.get(&"message_id");
         entry.role_id = map.get(&"role_id");
 
         return entry;
+    }
+
+    pub fn set_guild(connection: redis::RedisResult<Connection>, db_entry: DB_Entry) {
+        let mut map = HashMap::new();
+
+        map.insert("guild_id", entry.guild_id);
+        map.insert("message_id", entry.message_id);
+        map.insert("role_id", entry.role_id);
+
+        connection.set(map);
     }
 
 }
